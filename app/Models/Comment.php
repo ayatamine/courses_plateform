2e93689e807fc6a97a8e264f1fe78b027ce1186e
ayatamine <ayatir04@gmail.com>
2021-04-04 18:09:06 +0100

    starting changing the models to separate tutorials / courses

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,11 +2,26 @@
 
 namespace App\Models;
 
+use App\Models\Post;
+use App\Models\User;
+use App\Models\Video;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 // use App\Traits\SelfReference;
 class Comment extends Model
 {
     use HasFactory ;
-    protected $fillable=['content','user_id','vote_number','parent_id'];
+    protected $fillable=['content','user_id','commentable_id','commentable_type','vote_number','parent_id'];
+    public function commentable(){
+        return $this->morphTo();
+    }
+    public function user(){
+        return $this->belongsTo(User::class)->withDefault();
+    }
+    public function post(){
+        return $this->belongsTo(Post::class)->withDefault();
+    }
+    public function video(){
+        return $this->belongsTo(Video::class)->withDefault();
+    }
 }
